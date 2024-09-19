d9423139b2675458a9e980b44f1be2f8c1e97e7c
Willian06 <126635903+Willian06@users.noreply.github.com>
2024-09-19 15:03:43 -0300

    s

--- Gráficos/quantidadeUsuarios.js
+++ Gráficos/quantidadeUsuarios.js
@@ -4,8 +4,8 @@ async function quantidadeUsuarios() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
-    const nomeDasRedes = ['Quilombolas','casa',]
-    const quantidadeUsuarios = ['7666','890']
+    const nomeDasRedes = ['Quilombolas','indígenas',]
+    const quantidadeUsuarios = ['7666','305']
     const data = [
         {
             x: nomeDasRedes,
