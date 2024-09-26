82d4403c245556edcde627618fc2a791bee6dccc
Willian06 <126635903+Willian06@users.noreply.github.com>
2024-09-26 15:19:47 -0300

    g

--- Gráficos/informacoesGlobais.js
+++ Gráficos/informacoesGlobais.js
@@ -10,15 +10,16 @@ async function visualizarInformacoesGlobais() {
     const porcentagemConectada = ((pessoasConectadas/pessoasMundo)*100).toFixed(2)
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Política: Cerca de <span> 24% </span> dos deputados federais eleitos em 2022 se autodeclararam negros.
+    paragrafo.innerHTML = `Política: Cerca de <span>24%</span> dos deputados federais eleitos em 2022 se autodeclararam negros.
 
-Educação Superior: Aproximadamente 12% dos professores universitários se declaram negros, e 20% dos alunos em instituições de ensino superior são negros.
+Educação Superior: Aproximadamente <span>12%</span> dos professores universitários se declaram negros, e <span>20%</span> dos alunos em instituições de ensino superior são negros.
 
-Gerência Executiva: Estima-se que apenas 5% dos cargos de gerência executiva em empresas brasileiras sejam ocupados por pessoas negras.
+Gerência Executiva: Estima-se que apenas <span>5%</span> dos cargos de gerência executiva em empresas brasileiras sejam ocupados por pessoas negras.
 
-Entretenimento e Mídia: Aproximadamente 30% das pessoas negras estão representadas em papéis de destaque na mídia e entretenimento.
+Entretenimento e Mídia: Aproximadamente <span>30%</span> das pessoas negras estão representadas em papéis de destaque na mídia e entretenimento.
 
-Professores: Cerca de 35% dos professores na educação básica se autodeclaram negros.
+Professores: Cerca de <span>35%</span> dos professores na educação básica se autodeclaram negros.
+`
  `
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
