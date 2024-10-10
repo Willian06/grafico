de1fabb7e2d05fe420f1ecaa47d4e12183784cef
Willian06 <126635903+Willian06@users.noreply.github.com>
2024-10-10 13:44:02 -0300

    e

--- Gráficos/grafico-2.js
+++ Gráficos/grafico-2.js
@@ -4,8 +4,8 @@ async function quantidadeUsuarios() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
-    const nomeDasRedes = ['Política','Educação Superior','Gerência Executiva','Entreterimento e mídia','Professores']
-    const quantidadeUsuarios = ['24%','12%','5%','30%','35%']
+    const nomeDasRedes = ['15 anos','24anos','25 anos','54 anos','55 anos ou mais']
+    const quantidadeUsuarios = ['10','10','50','50','40','40']
     const data = [
         {
             x: nomeDasRedes,
